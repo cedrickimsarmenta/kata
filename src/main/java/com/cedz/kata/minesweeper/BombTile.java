c99545d74f53fcbd99c7ea2ff41248674b3b7542
Cedric Sarmenta <cedric.sarmenta@synpulse.com>
2020-03-03 11:27:37 +0800

    Count diagonals as part of neighbors

--- src/main/java/com/cedz/kata/minesweeper/BombTile.java
+++ src/main/java/com/cedz/kata/minesweeper/BombTile.java
@@ -16,17 +16,38 @@ public class BombTile extends AbstractTile {
   }
 
   public void updateNeighbors(Tile[][] board) {
-    if(y > 0) {
+    boolean notLastColumn =  this.y < board[x].length - 1;
+    boolean notLastRow =this.x < board.length - 1;
+    boolean notFirstRow = x > 0;
+    boolean notFirstColumn = y > 0;
+    if(notFirstColumn) {
       board[x][y-1].incrementBombCount();
     }
-    if(y < board[x].length - 1) {
+    if(notLastColumn) {
       board[x][y+1].incrementBombCount();
     }
-    if(x > 0) {
+    if(notFirstRow) {
       board[x-1][y].incrementBombCount();
     }
-    if(x < board.length -1) {
+    if(notLastRow) {
       board[x+1][y].incrementBombCount();
     }
+    if(notFirstRow && notFirstColumn) {
+      board[x-1][y-1].incrementBombCount();
+    }
+    if(notLastRow && notLastColumn) {
+      board[x+1][y+1].incrementBombCount();
+    }
+    if(notLastRow && notFirstColumn) {
+      board[x+1][y-1].incrementBombCount();
+    }
+    if(notFirstRow && notLastColumn) {
+      board[x-1][y+1].incrementBombCount();
+    }
+
+
+
+
+
   }
 }
