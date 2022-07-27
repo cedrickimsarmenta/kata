be064055b7c4662d056ea4d9ce546f54753a63eb
Cedric Sarmenta <cedric@codapayments.com>
2022-07-27 11:34:35 +0800

    Insertion sort

---
+++ src/main/java/com/cedz/kata/sort/v2/InsertionSort.java
@@ -0,0 +1,28 @@
+package com.cedz.kata.sort.v2;
+
+import com.cedz.kata.sort.Sort;
+import com.cedz.kata.util.CollectionUtils;
+
+import java.util.List;
+
+public class InsertionSort implements Sort {
+
+    @Override
+    public void sort(List<Integer> list) {
+        if(CollectionUtils.isNullOrEmpty(list) || list.size() == 1) {
+            return;
+        }
+        for (int i = 1; i < list.size(); i ++) {
+
+            int k = i-1;
+            int temp  = list.get(i);
+
+            while(k >= 0 && list.get(k) > temp) {
+                list.set(k+1, list.get(k));
+                k--;
+            }
+
+            list.set(k +1 , temp);
+        }
+    }
+}
